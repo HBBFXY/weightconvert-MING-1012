2187b15bd136cc3d65163766327d36032e5e129c
MING-1012 <1130352416@qq.com>
2025-09-10 17:39:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def weight_convert():
     user_input = input("请输入带单位的重量:")
     if user_input.endswith('kg'):
       weight_kg = float(user_input[:-2])
-      weight_pd = weight_kg * 2.2046
+      weight_pd = weight_kg * 2.2049
       print(f"对应的英制重量为{weight_pd:.3f}磅")
     elif user_input.endswith('pd'):
       weight_pd = float(user_input[:-2])
